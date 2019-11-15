7655f59855da038ae50d90f2ec5acf43d878a3ac
Akhterul Mustafa <amustafa2020@gmail.com>
2019-11-15 10:59:41 -0600

    first commit

---
+++ Publisher/Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Publisher.Services;
+
+namespace Publisher.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IMessageService _messageService;
+        public WeatherForecastController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "MyQueue" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string payload)
+        {
+            Console.WriteLine("received a Post: " + payload);
+            _messageService.Enqueue(payload);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Publisher/Services/MessageService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher.Services
+{
+    public interface IMessageService
+    {
+        bool Enqueue(string message);
+    }
+
+    public class MessageService : IMessageService
+    {
+        ConnectionFactory _factory;
+        IConnection _conn;
+        IModel _channel;
+        public MessageService()
+        {
+            Console.WriteLine("about to connect to rabbit");
+
+            _factory = new ConnectionFactory() { HostName = "rabbitmq", Port = 5672 };
+            _factory.UserName = "guest";
+            _factory.Password = "guest";
+            _conn = _factory.CreateConnection();
+            _channel = _conn.CreateModel();
+            _channel.QueueDeclare(queue: "hello",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+        }
+        public bool Enqueue(string messageString)
+        {
+            var body = Encoding.UTF8.GetBytes("server processed " + messageString);
+            _channel.BasicPublish(exchange: "",
+                                routingKey: "hello",
+                                basicProperties: null,
+                                body: body);
+            Console.WriteLine(" [x] Published {0} to RabbitMQ", messageString);
+            return true;
+        }
+    }
+}
